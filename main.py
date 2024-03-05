211ea4d9c912b6c5d64cc4e1b7a5f5aab05ad753
IDGS-801-21000401 <21000401@alumnos.utleon.edu.mx>
2024-03-04 21:25:00 -0600

    04/03/24 ARMC Se realizó Modificar y Eliminar en Empleados

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,flash,g
+from flask import Flask,render_template,request,flash,g,redirect,url_for
 from forms import UserForm
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
@@ -49,6 +49,58 @@ def index():
 def ABCompleto():
     empleado = Empleados.query.all()
     return render_template("ABC_Completo.html", empleados=empleado)
+
+@app.route("/eliminar", methods=[ "GET", "POST"])
+def eliminar():
+    formulario = UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        empleado1 = db.session.query(Empleados).filter(Empleados.id == id).first()
+        formulario.id.data = request.args.get('id')
+        formulario.nombre.data = empleado1.nombre
+        formulario.direccion.data = empleado1.direccion
+        formulario.telefono.data = empleado1.telefono
+        formulario.sueldo.data = empleado1.sueldo
+        formulario.email.data = empleado1.email
+        
+    if request.method == 'POST':
+        id = formulario.id.data
+        empleado = Empleados.query.get(id)
+        db.session.delete(empleado)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+
+    return render_template("eliminar.html", form=formulario)
+
+
+@app.route("/modificar", methods=["GET", "POST"])
+def modificar():
+    formulario = UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alumno = db.session.query(Empleados).filter(Empleados.id == id).first()
+        formulario.id.data = id
+        formulario.nombre.data = alumno.nombre
+        formulario.direccion.data = alumno.direccion
+        formulario.telefono.data = alumno.telefono
+        formulario.email.data = alumno.email
+        formulario.sueldo.data = alumno.sueldo
+
+    if request.method == 'POST':
+        id = formulario.id.data
+        alumno = Empleados.query.get(id)
+        alumno.nombre = formulario.nombre.data
+        alumno.direccion = formulario.direccion.data
+        alumno.telefono = formulario.telefono.data
+        alumno.email = formulario.email.data
+        alumno.sueldo = formulario.sueldo.data
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+
+    return render_template("modificar.html", form=formulario)
+
+
+
 
 if __name__ == "__main__":
     csrf.init_app(app)
